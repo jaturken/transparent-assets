3433822b908e3235868fdd4600f814c416085174
Alex Teut <jaturken@gmail.com>
2013-01-20 01:02:53 +0300

    Added creation of initializer and config in installer

--- lib/generators/transparent_assets/install_generator.rb
+++ lib/generators/transparent_assets/install_generator.rb
@@ -1,7 +1,13 @@
 module TransparentAssets
   class InstallGenerator < Rails::Generators::Base
-    def do_test
-      print ActiveRecord.class
+    def create_initializer
+      initializer_text = "# Add initialization content here"
+      create_file "config/initializers/transparent_assets.rb", initializer_text
+    end
+
+    def create_config
+      config_text = "# Add config content here"
+      create_file "config/transparent_assets.yml", config_text
     end
   end
 end
