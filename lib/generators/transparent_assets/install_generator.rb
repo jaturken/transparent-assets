d28271f122accc6dbb2dabc3f27e006756aabfce
Alex Teut <jaturken@gmail.com>
2013-01-20 01:43:48 +0300

    Created templates for installation files

--- lib/generators/transparent_assets/install_generator.rb
+++ lib/generators/transparent_assets/install_generator.rb
@@ -1,14 +1,18 @@
 module TransparentAssets
   class InstallGenerator < Rails::Generators::Base
     desc "This generator creates initializer and config files for transparent_assets gem"
+    source_root File.expand_path("../templates", __FILE__)
+    
     def create_initializer
-      initializer_text = "# Add initialization content here"
-      create_file "config/initializers/transparent_assets.rb", initializer_text
+      template "initializer.rb", "config/initializers/transparent_assets.rb"
     end
 
     def create_config
-      config_text = "# Add config content here"
-      create_file "config/transparent_assets.yml", config_text
+      template "config.yml", "config/transparent_assets.yml"
+    end
+
+    def add_uuid_model
+      template  "uuid.rb", "app/models/uuid.rb"
     end
   end
 end
