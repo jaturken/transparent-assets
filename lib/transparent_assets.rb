32b17491db4057cd3c0dd90f5bf7383a45062a54
Alex Teut <jaturken@gmail.com>
2013-01-19 18:53:44 +0300

    Added generation og uuids

--- lib/transparent_assets.rb
+++ lib/transparent_assets.rb
@@ -1,5 +1,14 @@
-class TransparentAssets
+require "uuid"
+
+module TransparentAssets
+
   def self.test
     "Transparent Assets gem."
   end
+
+  def self.generate_uuid
+    uuid = UUID.new
+    uuid.generate
+  end
+
 end
